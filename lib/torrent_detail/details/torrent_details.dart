3d20794b05bd4b6577cf1d89a2f31538ee6fede8
drolevar <14980349+drolevar@users.noreply.github.com>
2026-08-10 13:11:28 +0000

    fix(ui): refactor torrent details layout to prevent text overflow

--- lib/torrent_detail/details/torrent_details.dart
+++ lib/torrent_detail/details/torrent_details.dart
@@ -369,7 +369,7 @@ class _TorrentStatus extends StatelessWidget {
         TorrentStateProperties(getTorrentStateForStateString(torrentDetail.state, torrentDetail.isFinished)).color;
     return Center(
         child: SizedBox(
-            width: 208.0,
+            width: 240.0,
             child: AspectRatio(
                 aspectRatio: 1.0,
                 child: Stack(
@@ -397,82 +397,91 @@ class _TorrentStatus extends StatelessWidget {
 
   Widget _buildTorrentStatusContent(BuildContext context) {
     var formatter = ByteSizeFormatter.of(PreferenceProvider.of(context).byteSizeStyle);
-    return Column(children: <Widget>[
-      Container(
-        height: 16.0,
-      ),
-      Text(
-        controller.getProgressPercentage(),
-        style: const TextStyle(fontSize: 24.0),
-      ),
-      Text(torrentDetail.state),
-      Container(
-        height: 8.0,
-      ),
-      Row(
+    return FittedBox(
+      fit: BoxFit.scaleDown,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          Expanded(
-            child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: <Widget>[
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  const Icon(
-                    Icons.arrow_downward,
-                    size: 14.0,
-                  ),
-                  Text(Strings.detailDown),
-                ],
-              ),
-              Text(
-                controller.getDoneSize(formatter),
-                style: const TextStyle(fontSize: 22.0),
-              ),
-              Text("of ${controller.getWantedSize(formatter)}"),
-              Text(
-                controller.getDownloadSpeed(formatter),
-                style: const TextStyle(fontSize: 16.0),
-              ),
-              Text("${Strings.detailSeeds} ${torrentDetail.connectedSeeds}"),
-              Text(
-                "of ${torrentDetail.totalSeeds}",
-                style: const TextStyle(fontSize: 12.0),
-              )
-            ]),
-          ),
           Container(
-            width: 4.0,
+            height: 16.0,
           ),
-          Expanded(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          Text(
+            controller.getProgressPercentage(),
+            style: const TextStyle(fontSize: 24.0),
+          ),
+          Text(torrentDetail.state),
+          Container(
+            height: 8.0,
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              Row(
-                children: <Widget>[
-                  const Icon(
-                    Icons.arrow_upward,
-                    size: 14.0,
-                  ),
-                  Text(Strings.detailUp),
-                ],
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        const Icon(
+                          Icons.arrow_downward,
+                          size: 14.0,
+                        ),
+                        Text(Strings.detailDown),
+                      ],
+                    ),
+                    Text(
+                      controller.getDoneSize(formatter),
+                      style: const TextStyle(fontSize: 22.0),
+                    ),
+                    Text("of ${controller.getWantedSize(formatter)}"),
+                    Text(
+                      controller.getDownloadSpeed(formatter),
+                      style: const TextStyle(fontSize: 16.0),
+                    ),
+                    Text(
+                        "${Strings.detailSeeds} ${torrentDetail.connectedSeeds}"),
+                    Text(
+                      "of ${torrentDetail.totalSeeds}",
+                      style: const TextStyle(fontSize: 12.0),
+                    )
+                  ]),
+              Container(
+                width: 16.0,
               ),
-              Text(
-                controller.getUploadedSize(formatter),
-                style: const TextStyle(fontSize: 22.0),
-              ),
-              Text("${Strings.detailRatioLabel} ${controller.getRatio()}"),
-              Text(
-                controller.getUploadSpeed(formatter),
-                style: const TextStyle(fontSize: 16.0),
-              ),
-              Text("${Strings.detailPeers} ${torrentDetail.connectedPeers}"),
-              Text(
-                "of ${torrentDetail.totalPeers}",
-                style: const TextStyle(fontSize: 12.0),
-              )
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        const Icon(
+                          Icons.arrow_upward,
+                          size: 14.0,
+                        ),
+                        Text(Strings.detailUp),
+                      ],
+                    ),
+                    Text(
+                      controller.getUploadedSize(formatter),
+                      style: const TextStyle(fontSize: 22.0),
+                    ),
+                    Text("${Strings.detailRatioLabel} ${controller.getRatio()}"),
+                    Text(
+                      controller.getUploadSpeed(formatter),
+                      style: const TextStyle(fontSize: 16.0),
+                    ),
+                    Text(
+                        "${Strings.detailPeers} ${torrentDetail.connectedPeers}"),
+                    Text(
+                      "of ${torrentDetail.totalPeers}",
+                      style: const TextStyle(fontSize: 12.0),
+                    )
+                  ])
             ],
-          ))
+          ),
         ],
       ),
-    ]);
+    );
   }
 }
